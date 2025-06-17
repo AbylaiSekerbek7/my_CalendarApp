4a9180679ba14b5de92587078a1e8331ed8bc52f
Abylai <221714@astanait.edu.kz>
2025-06-17 23:43:24 +0200

    fixing errors with event and participants

--- Calendar.Domain/obj/Debug/net9.0/Calendar.Domain.AssemblyInfo.cs
+++ Calendar.Domain/obj/Debug/net9.0/Calendar.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calendar.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+792fc3fefd879a9c658e6d6df034f8c65e54b10a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2dc8593d7ed2b85ae6af6ee16249eaf6c5308d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calendar.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calendar.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Calendar.WebApi/Program.cs
+++ Calendar.WebApi/Program.cs
@@ -1,20 +1,33 @@
 using Microsoft.EntityFrameworkCore;
 using Calendar.Infrastructure;
+using Calendar.Application.Interfaces;
+using Calendar.Application.Services; // Здесь должна быть реализация ParticipantService
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Подключение EF Core с SQLite
 builder.Services.AddDbContext<CalendarDbContext>(options =>
     options.UseSqlite("Data Source=calendar.db"));
 
-builder.Services.AddControllers();
+// Регистрация сервисов
+builder.Services.AddScoped<IParticipantService, ParticipantService>();
+
+// Добавление контроллеров и Swagger
+builder.Services.AddControllers()
+.AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+});;
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Подключение Swagger
 app.UseSwagger();
 app.UseSwaggerUI();
 
+// Промежуточное ПО
 app.UseAuthorization();
 app.MapControllers();
 
